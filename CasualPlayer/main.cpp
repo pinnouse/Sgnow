865f6801e152b3c15410ce37a8a1f87f6fd0273b
pinnouse <pinnouse@users.noreply.github.com>
2019-03-04 18:16:21 -0500

    Switch to SDL

--- CasualPlayer/main.cpp
+++ CasualPlayer/main.cpp
@@ -1,52 +1,106 @@
-#include <SFML/Graphics.hpp>
+//Using SDL and standard IO
+#include <SDL.h>
 #include <stdio.h>
 
-void renderingThread(sf::RenderWindow* window) {
-	window->setActive(true);
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
-	sf::Font font;
-	if (!font.loadFromFile("Comfortaa-Regular.ttf")) {
-		printf("Error: font not found");
+//Global variables
+SDL_Window* gWindow = NULL;
+
+SDL_Surface* gScreenSurface = NULL;
+
+SDL_Surface* gHelloWorld = NULL;
+
+bool init() {
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
 	}
-	printf("Loaded font");
-
-	sf::Text text;
-	text.setFont(font);
-	text.setString("Hello World");
-	text.setCharacterSize(28);
-	text.setFillColor(sf::Color::White);
-
-	while (window->isOpen()) {
-		window->clear(sf::Color::Color(64, 64, 64));
-		window->draw(shape);
-
-		window->draw(text);
-		window->display();
+	else {
+		//Create window
+		gWindow = SDL_CreateWindow("SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL) {
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else {
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
 	}
+
+	return success;
 }
 
-int main() {
+bool loadMedia() {
+	//Loading success flag
+	bool success = true;
 
-	sf::ContextSettings settings;
-	settings.antialiasingLevel = 14;
-
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Test", sf::Style::Default, settings);
-
-	window.setActive(false);
-
-	sf::Thread renderThread(&renderingThread, &window);
-	renderThread.launch();
-
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-		
+	//Load splash image
+	gHelloWorld = SDL_LoadBMP("02_getting_an_image_on_the_screen/hello_world.bmp");
+	if (gHelloWorld == NULL) {
+		printf("Unable to load image %s! SDL Error: %s\n", "hello_world.bmp", SDL_GetError());
+		success = false;
 	}
+
+	return success;
+}
+
+void close() {
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+int main(int argc, char* args[]) {
+
+	if (!init()) {
+		printf("Failed to initialize!\n");
+	}
+	else {
+		//Load media
+		if (!loadMedia()) {
+			printf("Failed to load media!\n");
+		}
+		else {
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+
+			//While application is running
+			while (!quit) {
+				//Handle events on queue
+				while (SDL_PollEvent(&e) != 0) {
+					//User requests quit
+					if (e.type == SDL_QUIT) {
+						quit = true;
+					}
+				}
+
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+
+			//Update the surface
+			SDL_UpdateWindowSurface(gWindow);
+			}
+		}
+	}
+
+	//Free resources and close SDL
+	close();
 
 	return 0;
 }
